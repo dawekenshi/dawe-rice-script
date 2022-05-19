46d6469966e65f8d847b32f35078ba4dcfc8cb21
dawekenshi <dawekenshi@keemail.me>
2022-05-19 15:19:27 +0200

    deleting output

--- dwmscript.sh
+++ dwmscript.sh
@@ -2,14 +2,14 @@
 
 echo "Downloading stuff and updating"
 sudo pacman -Syyuu > /dev/null
-sudo pacman -S base-devel picom /dev/null 
+sudo pacman -S base-devel picom > /dev/null 
 echo "Cloning and making dwm"
-git clone https://github.com/dawekenshi/suckless-dawe
+git clone https://github.com/dawekenshi/suckless-dawe > /dev/null
 cd suckless-dawe/dwm/
-sudo make clean install
+sudo make clean install > /dev/null
 cd ../dmenu/
-sudo make clean install
+sudo make clean install > /dev/null
 cd ../st/
-sudo make clean install
+sudo make clean install > /dev/null
 cd
 echo "exec dwm" > .xinitrc
