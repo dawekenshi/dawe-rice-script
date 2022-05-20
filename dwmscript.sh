fd8528ceab5b175e4469d6e6647a46b1fcadb3f5
dawekenshi <dawe@DESKTOP-4VTU2BG.localdomain>
2022-05-20 23:09:39 +0200

    debian support + progress bar

--- dwmscript.sh
+++ dwmscript.sh
@@ -1,44 +1,40 @@
 #! /bin/bash
 
-# For arch use this
-echo "[+] installing yay"
-cd
-git clone https://aur.archlinux.org/yay.git > /dev/null
-cd yay/
-makepkg -sir
-cd
-echo "[+] Downloading stuff and updating"
-sudo pacman -S base-devel picom feh make > /dev/null
-# For debian based use this
-# Soon
-echo "[+] Finished"
+# FOR ARCH USE THIS
+#cd
+#git clone https://aur.archlinux.org/yay.git > /dev/null
+#cd yay/
+#makepkg -sir
+#cd
+#sudo pacman -S base-devel picom feh make > /dev/null
 
-echo "[+] Cloning and making dwm"
+
+# FOR DEBIAN USE THIS
+echo "[+] Downloading stuff"
+cd
+echo "[#####             ] 25%"
+echo "[##########        ] 50%"
+sudo apt-get install make compton feh libx11-dev libxft-dev libxinerama-dev 
+echo "[###############   ] 75%"
+echo "[##################] 100%"
+
+echo "[+] Starting with the cloning and making"
+echo "[###               ] 16%"
 git clone https://github.com/dawekenshi/suckless-dawe > /dev/null
 cd suckless-dawe/dwm/
-echo "[+] Finished"
-
-echo "[+] Making dwm"
+echo "[######            ] 32%"
 sudo make clean install > /dev/null
 cd ../dmenu/
-echo "[+] Finished"
-
-echo "[+] Making dmenu"
+echo "[#########         ] 48%"
 sudo make clean install > /dev/null
 cd ../st/
-echo "[+] Finished"
-
-echo "[+] Making st"
+echo "[############      ] 64%"
 sudo make clean install > /dev/null
 cd
-echo "[+] Finished"
-
-echo "[+] Configuring background"
+echo "[###############   ] 80%"
 cd dawe-rice-script/backgrounds/
 feh --bg-fill  jojos-winxp.jpg
-echo "[+] Finished"
-
-echo "[+] Configuring .xinitrc"
+echo "[##################] 100%"
 cd ..
 mv xinit-copy ~/.xinitrc
 echo "[+] Finished, enjoy your dwm!"
