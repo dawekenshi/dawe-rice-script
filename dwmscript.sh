f9ca3719be9b46612bd7391afa181e9b5fb8a2a1
dawekenshi <dawekenshi@keemail.me>
2022-05-20 14:06:38 +0200

    background added

--- dwmscript.sh
+++ dwmscript.sh
@@ -2,36 +2,45 @@
 
 echo "[+] Downloading stuff and updating"
 # For arch use this
-#sudo pacman -Syyuu > /dev/null
-#sudo pacman -S base-devel picom > /dev/null
+sudo pacman -S base-devel picom feh > /dev/null
 # For debian based use this
-#sudo apt-get upgrade
-#sudo apt-get update
+# Soon
 sudo apt install make compton
 sleep 2
 echo "[+] Finished"
+
 echo "[+] Cloning and making dwm"
 cd
 git clone https://github.com/dawekenshi/suckless-dawe > /dev/null
 cd suckless-dawe/dwm/
 sleep 5
 echo "[+] Finished"
+
 echo "[+] Making dwm"
 sudo make clean install > /dev/null
 cd ../dmenu/
 sleep 5
 echo "[+] Finished"
+
 echo "[+] Making dmenu"
 sudo make clean install > /dev/null
 cd ../st/
 sleep 5
 echo "[+] Finished"
+
 echo "[+] Making st"
 sudo make clean install > /dev/null
 cd
 sleep 5
 echo "[+] Finished"
-echo "[+] Adding dwm to .xinitrc"
-echo "exec dwm" > .xinitrc
-sleep 5
+
+echo "[+] Configuring background"
+cd dawe-rice-script/backgrounds/
+feh --bg-fill  jojos-winxp.jpg
 echo "[+] Finished"
+
+echo "[+] Configuring .xinitrc"
+cd ..
+mv xinit-copy ~/.xinitrc
+sleep 5
+echo "[+] Finished, enjoy your dwm!"
