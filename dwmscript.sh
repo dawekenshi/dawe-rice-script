9d2d118aac933743985d417db398c0261b778657
dawekenshi <dawekenshi@keemail.me>
2022-05-20 14:21:36 +0200

    2.0

--- dwmscript.sh
+++ dwmscript.sh
@@ -2,10 +2,9 @@
 
 echo "[+] Downloading stuff and updating"
 # For arch use this
-sudo pacman -S base-devel picom feh > /dev/null
+sudo pacman -S base-devel picom feh make > /dev/null
 # For debian based use this
 # Soon
-sudo apt install make compton
 sleep 2
 echo "[+] Finished"
 
@@ -13,25 +12,25 @@ echo "[+] Cloning and making dwm"
 cd
 git clone https://github.com/dawekenshi/suckless-dawe > /dev/null
 cd suckless-dawe/dwm/
-sleep 5
+sleep 2
 echo "[+] Finished"
 
 echo "[+] Making dwm"
 sudo make clean install > /dev/null
 cd ../dmenu/
-sleep 5
+sleep 2
 echo "[+] Finished"
 
 echo "[+] Making dmenu"
 sudo make clean install > /dev/null
 cd ../st/
-sleep 5
+sleep 2
 echo "[+] Finished"
 
 echo "[+] Making st"
 sudo make clean install > /dev/null
 cd
-sleep 5
+sleep 2
 echo "[+] Finished"
 
 echo "[+] Configuring background"
@@ -42,5 +41,5 @@ echo "[+] Finished"
 echo "[+] Configuring .xinitrc"
 cd ..
 mv xinit-copy ~/.xinitrc
-sleep 5
+sleep 2
 echo "[+] Finished, enjoy your dwm!"
